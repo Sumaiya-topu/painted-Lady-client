a8700811d963fbdd7a87d05481d1e2954f7dc02c
Sumaiya-topu <topusumaiya@gmail.com>
2022-11-08 13:31:48 +0600

    project set up

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,11 @@
+const { createBrowserRouter } = require("react-router-dom");
+
+//create router
+const router = createBrowserRouter([
+    {
+        path: '/',
+
+    }
+])
+
+export default router;
